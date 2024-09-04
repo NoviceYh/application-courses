efbca12c1aed3d8f82d901d038788452d5ac5907
Alejandro Aguirre <aguirreee21@gmail.com>
2024-09-04 20:00:33 -0300

    Adding Repositories

---
+++ src/main/java/com/application/courses/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.application.courses.repository;
+
+import com.application.courses.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
