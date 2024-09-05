09aac0e768137702a406ca410a1250ee599571a2
Alejandro Aguirre <aguirreee21@gmail.com>
2024-09-04 22:33:39 -0300

    Creating courses

--- src/main/java/com/application/courses/service/CourseService.java
+++ src/main/java/com/application/courses/service/CourseService.java
@@ -1,5 +1,13 @@
 package com.application.courses.service;
 
+import com.application.courses.model.Course;
+
+import java.util.List;
 
 public interface CourseService {
+
+    Course createCourse(Course course);
+
+    List<Course> findAllCourses();
+
 }
